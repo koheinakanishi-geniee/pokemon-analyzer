2addfbf0afc342f9f98467ddefcc37b087fbed30
koheinakanishi-geniee <kohei.nakanishi@geniee.co.jp>
2020-11-02 13:05:19 +0900

    あ

--- app/src/pages/name/[name]/index.tsx
+++ app/src/pages/name/[name]/index.tsx
@@ -1,12 +1,17 @@
 import {NextPage} from "next";
 import {useRouter} from "next/router";
+import React from "react";
 
 const Page: NextPage = () => {
     const router = useRouter();
     const { name } = router.query;
     return (
-        <script src={"https://log.ma-jin.jp/ma.js?acid=1130"}></script>
-        <p>{name}</p>
-    )
+        <>
+            <div>
+                <script src={"https://log.ma-jin.jp/ma.js?acid=1130"}/>
+                <p>{name}</p>
+            </div>
+        </>
+    );
 };
 export default Page;
